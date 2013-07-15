1117f74d91705a8fa5f9d66c526dc4e65dd1854b
Clochix <clochix@clochix.net>
2013-07-15 21:26:45 +0200

    Refactoring

--- js/alir.js
+++ js/alir.js
@@ -1,5 +1,23 @@
 /*jshint browser: true, devel: true */
 /*global remoteStorage: true*/
+/**
+ * @TODO handle events
+ * 'busy'
+ * 'change'
+ * 'conflict'
+ * 'connect'
+ * 'connected'
+ * 'disconnect'
+ * 'disconnected'
+ * 'error'
+ * 'ready'
+ * 'reconnect'
+ * 'state'
+ * 'sync'
+ * 'timeout'
+ * 'unbusy'
+ */
+
 var list = document.getElementById('list'),
     listHtml = '';
 remoteStorage.defineModule('alir', function module(privateClient, publicClient) {
@@ -28,9 +46,13 @@ remoteStorage.defineModule('alir', function module(privateClient, publicClient) 
 
   return {
     exports: {
-      add: function (trans) {
-        var obj = privateClient.buildObject('article', trans);
+      addPrivate: function (article) {
+        var obj = privateClient.buildObject('article', article);
         return privateClient.storeObject('article', obj.id, obj);
+      },
+      addPublic: function (article) {
+        var obj = publicClient.buildObject('article', article);
+        return publicClient.storeObject('article', obj.id, obj);
       },
       goOffline: function () {
         privateClient.release('');
@@ -45,90 +67,155 @@ remoteStorage.defineModule('alir', function module(privateClient, publicClient) 
     }
   };
 });
+// Common utilities {{{
 function format(str) {
   "use strict";
   var params = Array.prototype.splice.call(arguments, 1);
   return (str.replace(/%s/g, function () {return params.shift(); }));
 }
+// }}}
 function updateList() {
   "use strict";
-  remoteStorage.alir.private.getAll('').then(function onAll(objects) {
-    listHtml = '';
-    Object.keys(objects).forEach(function (key) {
-      var obj   = objects[key],
-          title = obj.title || key;
-      listHtml += format('<li data-key="%s"><h2>%s</h2>', key, title);
-      listHtml += '<p class="actions">';
-      listHtml += format('<a class="button" data-key="%s" data-action="toggle" href="#">Toggle</a> ', key);
-      listHtml += format('<a class="button" data-key="%s" data-action="delete" href="#">Delete</a> ', key);
-      listHtml += '</p>';
-      listHtml += format('<div class="content hidden">%s</div></li>', obj.html);
+  remoteStorage.alir.private.getAll('').then(function onAll(objectsPrivate) {
+    remoteStorage.alir.public.getAll('').then(function onAll(objectsPublic) {
+      function createList(objects, context) {
+        Object.keys(objects).forEach(function (key) {
+          var obj   = objects[key],
+              title = obj.title || key;
+          listHtml += format('<li data-key="%s" data-context="%s" class="%s"><h2>%s</h2>', key, context, context, title);
+          listHtml += '<p class="actions">';
+          listHtml += format('<a class="button" data-key="%s" data-action="toggle" href="#">Toggle</a> ', key);
+          listHtml += format('<a class="button" data-key="%s" data-action="delete" href="#">Delete</a> ', key);
+          listHtml += '</p>';
+          listHtml += format('<div class="content hidden"><a class="url" href="%s" target="_blank">%s</a><div class="html">%s</div></div></li>', obj.url || '#', obj.url || '', obj.html);
+        });
+      }
+      listHtml = '';
+      createList(objectsPublic, 'public');
+      createList(objectsPrivate, 'private');
+      list.innerHTML = listHtml;
     });
-    list.innerHTML = listHtml;
   });
 }
 function initUI() {
   "use strict";
-  list.addEventListener('click', function onClick(event) {
+  var $  = function (sel) {return document.querySelector.call(document, sel); },
+      $$ = function (sel) {return document.querySelectorAll.call(document, sel); },
+      forElement = function (sel, fct) {Array.prototype.forEach.call(document.querySelectorAll(sel), fct); },
+      UI = {},
+      menuActions = {};
+  UI = {
+    input: $('#input'),
+    list: $('#list'),
+    menu: {}
+  };
+  // left menu actions
+  menuActions = {
+    create: function doCreate() {
+      UI.list.classList.toggle('hidden');
+      UI.input.classList.toggle('hidden');
+      menuActions.toggleMenu();
+    },
+    toggleContent: function doToggle() {
+      forElement('#list .content', function (e) {
+        e.classList.add('hidden');
+      });
+      forElement('#list li[data-key]', function (e) {
+        e.classList.remove('hidden');
+      });
+      UI.menu.toggleContent.classList.toggle('hidden');
+      menuActions.toggleMenu();
+    },
+    toggleMenu: function doToggleMenu() {
+      $('#menu').classList.toggle("show");
+      document.body.classList.toggle("menu");
+    },
+    sync: function doSync() {
+      document.body.classList.add('sync');
+      remoteStorage.fullSync().then(function onSyncDone() {
+        document.body.classList.remove('sync');
+        window.alert('Synk ok');
+      }, function onSynFail() {
+        document.body.classList.remove('sync');
+        window.alert('Synk ko');
+      });
+    },
+    offline: function doOffline() {
+      remoteStorage.alir.goOffline();
+      UI.menu.offline.classList.toggle('hidden');
+      UI.menu.online.classList.toggle('hidden');
+      document.body.classList.toggle('online');
+    },
+    online: function doOnline() {
+      remoteStorage.alir.goOnline();
+      UI.menu.online.classList.toggle('hidden');
+      UI.menu.offline.classList.toggle('hidden');
+      document.body.classList.toggle('online');
+    }
+  };
+  forElement('#menu .content [data-action]', function () {
+    var elmt = arguments[0];
+    UI.menu[elmt.dataset.action] = elmt;
+  });
+
+  UI.list.addEventListener('click', function onClick(event) {
     var target = event.target,
-        key;
+        context, key;
     if (target.dataset.action) {
       key = target.dataset.key;
+      context = target.parentNode.parentNode.dataset.context;
       switch (target.dataset.action) {
       case 'toggle':
-        Array.prototype.forEach.call(document.querySelectorAll('li[data-key]'), function (e) {
+        Array.prototype.forEach.call($$('li[data-key]'), function (e) {
           e.classList.toggle('hidden');
         });
-        document.querySelector('[data-key="' + key + '"]').classList.toggle('hidden');
-        document.querySelector('[data-key="' + key + '"] .content').classList.toggle('hidden');
-        document.querySelector('#menu .toggle').classList.toggle('hidden');
+        $('[data-key="' + key + '"]').classList.toggle('hidden');
+        $('[data-key="' + key + '"] .content').classList.toggle('hidden');
+        UI.menu.toggleContent.classList.toggle('hidden');
         break;
       case 'delete':
         if (window.confirm("Supprimer ???")) {
-          remoteStorage.alir.private.remove(key);
+          remoteStorage.alir[context].remove(key);
         }
       }
     }
   });
-  // Left menu
-  document.querySelector('#menu .slider').addEventListener('click', function toggleMenu() {
-    document.querySelector('#menu').classList.toggle("show");
+  // input {{
+  $('#input [name="public"]').addEventListener('click', function () {
+    var obj = {
+      id: $('#input [name="id"]').value,
+      title: $('#input [name="title"]').value,
+      html: $('#input [name="html"]').value
+    };
+    remoteStorage.alir.addPublic(obj);
+    menuActions.create();
   });
-  document.querySelector('#menu .toggle').addEventListener('click', function menuToggle() {
-    Array.prototype.forEach.call(document.querySelectorAll('#list .content'), function (e) {
-      e.classList.add('hidden');
-    });
-    Array.prototype.forEach.call(document.querySelectorAll('#list li[data-key]'), function (e) {
-      e.classList.remove('hidden');
-    });
-    document.querySelector('#menu .toggle').classList.toggle('hidden');
-    document.querySelector('#menu').classList.toggle("show");
+  $('#input [name="private"]').addEventListener('click', function () {
+    var obj = {
+      id: $('#input [name="id"]').value,
+      url: $('#input [name="url"]').value,
+      title: $('#input [name="title"]').value,
+      html: $('#input [name="html"]').value
+    };
+    remoteStorage.alir.addPrivate(obj);
+    menuActions.create();
   });
-  // Force sync
-  document.querySelector('#menu .sync').addEventListener('click', function menuSync() {
-    document.body.classList.add('sync');
-    remoteStorage.fullSync().then(function onSyncDone() {
-      document.body.classList.remove('sync');
-      window.alert('Synk ok');
-    }, function onSynFail() {
-      document.body.classList.remove('sync');
-      window.alert('Synk ko');
-    });
+  $('#input [name="cancel"]').addEventListener('click', function () {
+    UI.list.classList.toggle('hidden');
+    UI.input.classList.toggle('hidden');
   });
-  // Go offline
-  document.querySelector('#menu .offline').addEventListener('click', function menuSync() {
-    remoteStorage.alir.goOffline();
-    document.querySelector('#menu .offline').classList.toggle('hidden');
-    document.querySelector('#menu .online').classList.toggle('hidden');
-    document.body.classList.toggle('online');
+  // }}
+  // Left menu {{
+  $('#menu').addEventListener('click', function (event) {
+    var target = event.target,
+        action = target.dataset.action;
+    if (action && menuActions[action]) {
+      menuActions[action]();
+    } else {
+      console.log("Unknown action", target.dataset.action, menuActions);
+    }
   });
-  // Go online
-  document.querySelector('#menu .online').addEventListener('click', function menuSync() {
-    remoteStorage.alir.goOnline();
-    document.querySelector('#menu .offline').classList.toggle('hidden');
-    document.querySelector('#menu .online').classList.toggle('hidden');
-    document.body.classList.toggle('online');
-  });
+  // }}
 }
 // }}
 
